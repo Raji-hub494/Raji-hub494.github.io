8bf30257742a902c61848af0237dd57976adbc94
Raji-hub494 <79651172+Raji-hub494@users.noreply.github.com>
2022-04-28 20:56:13 +0530

    Update script.js

--- Module 4 solution/script.js
+++ Module 4 solution/script.js
@@ -1,19 +1,16 @@
-var names=new Array();
-names[0]="Yaakov";
-names[1]="John";
-names[2]="Jenn";
-names[3]="jagu";
-names[4]="Paul";
-names[5]="frank";
-names[6]="larry";
-names[7]="Paugan";
-names[8]="Jimy";
-names[9]="jim"; 
+(function () {
+
+var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
 
 for (var i = 0; i < names.length; i++) {
-	if(names[i].charAt(0)==='J'|| names[i].charAt(0)==='j'){
-        console.log("Goodbye "+ names[i])
-	}
-	else{
-		console.log("Hello "+ names[i])
-	}
+  var firstLetter = names[i].charAt(0).toLowerCase();
+
+  if (firstLetter === 'j') {
+    byeSpeaker(names[i]);
+  }
+  else {
+    helloSpeaker(names[i]);
+  }
+}
+
+})();
